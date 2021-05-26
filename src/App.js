29a4208d9041ef949ec6f0112352cf230fc62a95
ktauchert <ktauchert@users.noreply.github.com>
2021-05-26 08:01:46 +0200

    added about and router

--- src/App.js
+++ src/App.js
@@ -1,73 +1,109 @@
-import React from 'react';
-import Navbar from './component/layout/Navbar'
-import Alert from './component/layout/Alert'
-import Users from './component/users/Users';
-import Search from './component/users/Search';
-import axios from 'axios';
-import './App.css';
-
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./component/layout/Navbar";
+import Alert from "./component/layout/Alert";
+import Users from "./component/users/Users";
+import User from "./component/users/User";
+import Search from "./component/users/Search";
+import axios from "axios";
+import "./App.css";
+import About from "./component/pages/About";
 
 class App extends React.Component {
-  state = {
-    users: [],
-    loading: false,
-    alert: null
-  }
+    state = {
+        users: [],
+        user: {},
+        loading: false,
+        alert: null,
+    };
 
-  // async componentDidMount() {
-  //   this.setState({
-  //     loading: true
-  //   })
-  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    // search Github users
+    searchUsers = async (text) => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
 
-  //   this.setState({
-  //     users: res.data,
-  //     loading: false,
-  //   })
-  // }
+        this.setState({
+            users: res.data.items,
+            loading: false,
+        });
+    };
+    // get single github user
+    getUser = async (username) => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
 
-  // search Github users
-  searchUsers = async text => {
-    this.setState({ loading: true })
-    console.log(text);
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        this.setState({
+            user: res.data,
+            loading: false,
+        });
+    };
+    clearUsers = () => this.setState({ users: [], loading: false });
 
-    this.setState({
-      users: res.data.items,
-      loading: false,
-    })
-  }
-  clearUsers = () => this.setState({ users: [], loading: false })
+    setAlert = (message, type) => {
+        this.setState({
+            alert: {
+                message,
+                type,
+            },
+        });
+        setTimeout(() => {
+            this.setState({
+                alert: null,
+            });
+        }, 5000);
+    };
 
-  setAlert = (message, type) => {
-    this.setState({
-      alert: {
-        message, type
-      }
-    });
-    setTimeout(() => {
-      this.setState({
-        alert: null
-      })
-    }, 5000);
-  }
+    render() {
+        const { loading, users, user, alert } = this.state;
 
-  render() {
-    const {
-      loading, users, alert
-    } = this.state;
-
-    return (
-      <div>
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0} setAlert={this.setAlert} />
-          <Users loading={loading} users={users} />
-        </div>
-      </div>
-    );
-  }
+        return (
+            <Router>
+                <div>
+                    <Navbar />
+                    <div className="container">
+                        <Alert alert={alert} />
+                        <Switch>
+                            <Route
+                                exact
+                                path="/"
+                                render={(props) => (
+                                    <Fragment>
+                                        <Search
+                                            searchUsers={this.searchUsers}
+                                            clearUsers={this.clearUsers}
+                                            showClear={users.length > 0}
+                                            setAlert={this.setAlert}
+                                        />
+                                        <Users
+                                            loading={loading}
+                                            users={users}
+                                        />
+                                    </Fragment>
+                                )}
+                            />
+                            <Route exact path="/about" render={About} />
+                            <Route
+                                exact
+                                path="/user/:login"
+                                render={(props) => (
+                                    <User
+                                        {...props}
+                                        getUser={this.getUser}
+                                        user={user}
+                                        loading={loading}
+                                    />
+                                )}
+                            />
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/component/pages/About.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>Über diese App</h1>
+            <p>
+                Dies ist eine App (aus dem Kurs React FRont to Back von Traversy
+                Media), die mit Hilfe der Github API github-Bebutzer durchsucht
+                und Informationen anzeigt.
+            </p>
+            <p>Version 1.0.0</p>
+        </Fragment>
+    );
+};
+
+export default About;
